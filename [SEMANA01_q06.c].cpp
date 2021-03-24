734e4b716b54280a3dbdba5fd5ea95d17de62468
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-03-23 22:07:51 -0300

    Add files via upload

---
+++ [SEMANA01_q06.c].cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(){
+  int num,sei=0;
+  scanf("%i",&num);
+  if(num==0){
+    printf("Zero\n");sei=1;
+  }
+  if(num==1){
+    printf("One\n");sei=1;
+  }
+  if(num==2){
+    printf("Two\n");sei=1;
+  }
+  if(num==3){
+    printf("Three\n");sei=1;
+  }
+  if(num==4){
+    printf("Four\n");sei=1;
+  }
+  if(num==5){
+    printf("Five\n");sei=1;
+  }
+  if(num==6){
+    printf("Six\n");sei=1;
+  }
+  if(num==7){
+    printf("Seven\n");sei=1;
+  }
+  if(num==8){
+    printf("Eight\n");sei=1;
+  }
+  if(num==9){
+    printf("Nine\n");sei=1;
+  }
+  return 0;
+}
