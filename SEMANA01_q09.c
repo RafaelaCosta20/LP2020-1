1a6969435133840fdb2d8d8de49535ce80505200
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-04-08 09:16:02 -0300

    Add files via upload

---
+++ SEMANA01_q09.c
@@ -0,0 +1,42 @@
+/*#include <stdio.h>
+int main(){
+  int nota100,nota50,nota20,nota10,nota5,nota2,nota1,valor;
+  scanf("%d",&valor);
+  nota100 = valor / 100;
+  valor = valor % 100;
+  if(nota100>0){
+  printf("%i de notas de 100\n",nota100);
+  }
+  nota50= valor / 50;
+  valor= valor % 50;
+  if(nota50>0){
+  printf("%i de notas de 50\n",nota50);
+  }
+  nota20= valor / 20;
+  valor= valor % 20;
+  if(nota20>0){
+  printf("%i de notas de 20\n",nota20);
+  }
+  nota10= valor / 10;
+  valor= valor % 10;
+  if(nota10>0){
+  printf("%i de notas de 10\n",nota10);
+  }
+  nota5= valor / 5;
+  valor= valor % 5;
+  if(nota5>0){
+  printf("%i de notas de 5\n",nota5);
+  }
+  nota2= valor / 2;
+  valor= valor % 2;
+  if(nota2>0){
+  printf("%i de notas de 2\n",nota2);
+  }
+  nota1= valor /1;
+  valor= valor % 1;
+  if(nota1>0){
+  printf("%i de notas de 1\n",nota1);
+  }
+  return 0;
+}*/
+
