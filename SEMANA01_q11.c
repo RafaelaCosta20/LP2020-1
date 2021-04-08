5999db68558bf83c9a7dea078b7c78f642be2350
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-04-08 11:53:25 -0300

    Add files via upload

---
+++ SEMANA01_q11.c
@@ -0,0 +1,28 @@
+/*#include <stdio.h>
+int main(){
+  int n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12,n13,somapar,somaimpar,soma,multiplo;
+  scanf("%i %i %i %i %i %i %i %i %i %i %i %i %i",&n1,&n2,&n3,&n4,&n5,&n6,&n7,&n8,&n9,&n10,&n11,&n12,&n13);
+  //passo 1:
+  //impar:
+  somaimpar=(n2+n4+n6+n8+n10+n12)*3;
+  //impar
+  //passo 2:
+  //par:
+  somapar=(n3+n5+n7+n9+n11+n13);
+  //par
+  //passo 3:
+  somaimpar=somaimpar+n1;
+  somapar=somapar+n1;
+  n1=somaimpar+somapar/10;
+ /// printf("%i",soma);
+  //passo 4:
+  if(n1/10){
+    printf("N\n");
+  }else{
+    if(n1/10===n1){
+    printf("S\n");
+    }
+  }
+  return 0;
+}*/
+
