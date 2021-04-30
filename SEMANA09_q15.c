3a6ed79187d7f149ec7ab9de472b65cae7fe03bd
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-04-30 14:24:26 -0300

    Add files via upload

---
+++ SEMANA09_q15.c
@@ -0,0 +1,16 @@
+/*#include <stdio.h>
+int main(){
+  int i=0,temperatura=0;
+  do{
+    scanf("%i",&temperatura);
+    if(temperatura>1000){
+      i=1;
+    }
+    }while(temperatura>=0);
+    if(i==1){
+      printf("DEU RUIM\n");
+    }else{
+      printf("TURNO TRANQUILO\n");
+    }
+  return 0;
+}*/
