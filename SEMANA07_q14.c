d41295e596e63c2f9d7162996fd33772562d9277
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-04-30 10:57:48 -0300

    Add files via upload

---
+++ SEMANA07_q14.c
@@ -0,0 +1,20 @@
+/*#include <stdio.h>
+int main(){
+ int num,i=0;
+ do{
+   scanf("%i",&num);
+     if(num>0){
+     if(num>i){
+       printf("C");
+       i=num;
+     }else if(num<i){
+       printf("B");
+       i=num;
+     }else if(i==num){
+       printf("-");
+       i=num;
+     }
+    }
+ }while(num>=0);
+  return 0;
+}*/
