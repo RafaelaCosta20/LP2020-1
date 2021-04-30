eba01a314180f5e8eb535e576011bebd01d81379
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-04-30 11:48:25 -0300

    Add files via upload

---
+++ SEMANA07_q13.c
@@ -0,0 +1,25 @@
+/*#include <stdio.h>
+int main(void) {
+ int num,aux=-1,maior=-1,segundo=-1,terceiro=-1;
+ do{
+   scanf("%i",&num);
+     if(num>0){
+       if(num>maior){
+          aux=maior;
+          maior=num;
+          num=aux;
+       }
+       if(num>segundo){
+          aux=segundo;
+          segundo=num;
+          num=aux;
+       }
+       if(num>terceiro){
+            aux=terceiro;
+            terceiro=num;
+         }
+      }
+     }while(num!=0);
+    printf("%i %i %i\n",maior,segundo,terceiro);
+  return 0;
+}*/
