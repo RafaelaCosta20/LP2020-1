baf449140fb5ddea7ce34b4601ad1ef43171ba32
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-05-02 14:04:24 -0300

    Add files via upload

---
+++ SEMANA09_q16.c
@@ -0,0 +1,21 @@
+/*#include <stdio.h>
+int main(){
+int temper=0,j=0,i=0, valor[30],inicio=0;
+float media=0;
+        do{
+        scanf("%d",&valor[i]);
+        i++;
+        }while(valor[i-1]>0);
+        for(j=i-5;j<i;j++){
+          media+=valor[j-1];
+        }
+        media/=5;
+         if(media>1000.0){
+            temper=1;
+        }  
+    if(temper==0){
+        printf("TURNO TRANQUILO");
+    }else{
+        printf("DEU RUIM");
+    }
+}*/
