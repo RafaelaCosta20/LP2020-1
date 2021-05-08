ec1025340c6b8a921500d39e9bdd149c663cd54b
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-05-08 20:10:15 -0300

    Add files via upload

---
+++ SEMANA09_q17.c
@@ -0,0 +1,66 @@
+/*
+#include <stdio.h>
+int  main () {
+	
+  int num =-1,
+      zero = 0,
+      um = 0,
+      dois = 0,
+      tres = 0,
+      quatro = 0,
+      cinco = 0,
+      seis = 0,
+      sete = 0,
+      oito = 0,
+      nove = 0;
+
+  do{
+    scanf("%d", &num);
+
+    switch(num){
+      case 0: zero++; break;
+      case 1: um++; break;
+      case 2: dois++; break;
+      case 3: tres++; break;
+      case 4: quatro++; break;
+      case 5: cinco++; break;
+      case 6: seis++; break;
+      case 7: sete++; break;
+      case 8: oito++; break;
+      case 9: nove++; break;
+    }
+
+  }while(num >=0 );
+
+  if(zero > 0){
+    printf("0: %i\n",zero);
+  }
+  if(um > 0){
+    printf("1: %i\n",um);
+  }
+  if(dois > 0){
+    printf("2: %i\n",dois);
+  }
+  if(tres > 0){
+    printf("3: %i\n",tres);
+  }
+  if(quatro > 0){
+    printf("4: %i\n",quatro);
+  }
+  if(cinco > 0){
+    printf("5: %i\n",cinco);
+  }
+  if(seis > 0){
+    printf("6: %i\n",seis);
+  }
+  if(sete > 0){
+    printf("7: %i\n",sete);
+  }
+  if(oito > 0){
+    printf("8: %i\n",oito);
+  }
+  if(nove > 0){
+    printf("9: %i\n",nove);
+  }
+	return  0 ;
+}*/
