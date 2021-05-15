ec1525a8b174c84991636e2753774dfb068b5313
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-05-15 19:19:51 -0300

    Add files via upload

---
+++ SEMANA09_q20.c
@@ -0,0 +1,33 @@
+/*#include <stdio.h>
+int main(){
+int i,x, aux, v[10];
+  for(i=0;i<10;i++){
+    scanf("%i",&v[i]);
+  }
+  for(i=0;i<10;i++){
+    for(x=i+1;x<10;x++){
+      if(v[i]>v[x]){
+        aux=v[i];
+        v[i]=v[x];
+        v[x]=aux;
+      }
+    }
+  }
+  for(i=0;i<10;i++){
+    printf("%i ",v[i]);
+  }
+  printf("\n");
+  for(i=0;i<10;i++){
+    for(x=i+1;x<10;x++){
+      if(v[i]<v[x]){
+        aux=v[i];
+        v[i]=v[x];
+        v[x]=aux;
+      }
+    }
+  }
+  for(i=0;i<10;i++){
+    printf("%i ",v[i]);
+  }
+  return 0;
+}*/
