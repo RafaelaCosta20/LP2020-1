dae1bfaec8df89dda0cce06fcae899a208cdc9e7
RafaelaCosta20 <79933634+RafaelaCosta20@users.noreply.github.com>
2021-05-16 14:25:09 -0300

    Add files via upload

---
+++ SEMANA09_q22.c
@@ -0,0 +1,40 @@
+/*
+#include<stdio.h>
+#include<string.h>
+int main(){
+  char texto[50];
+  int i;
+  gets(texto);
+  for(i=0;i<50;i++){
+  if(texto[i]=='a'){
+    texto[i]='i';    
+    }else{
+    if(texto[i]=='A'){
+    texto[i]='I';    
+      }
+    }
+  if(texto[i]=='e'){
+    texto[i]='i';    
+    }else{
+    if(texto[i]=='E'){
+    texto[i]='I';    
+      }
+    }
+  if(texto[i]=='o'){
+    texto[i]='i';    
+    }else{
+    if(texto[i]=='O'){
+    texto[i]='I';    
+      }
+    }
+  if(texto[i]=='u'){
+    texto[i]='i';    
+    }else{
+    if(texto[i]=='U'){
+    texto[i]='I';    
+      }
+    }
+  }
+  puts(texto);
+	return 0;
+}*/
